be927e39494944037c3de0ba59047a4469dd8404
Yassine Trabelsi <yassine.trabelsi@esprit.tn>
2025-02-08 17:58:11 +0100

    Workshop n1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResidencesComponent } from './residences/residences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResidencesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
