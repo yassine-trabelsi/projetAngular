abc58fca827f9366e4e33d3368d1e19af7d83085
Yassine Trabelsi <yassine.trabelsi@esprit.tn>
2025-02-20 19:43:35 +0100

    afficher les détails de la résidence

--- src/app/residences/residence-details/residence-details.component.ts
+++ src/app/residences/residence-details/residence-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import { Residence } from 'src/core/models/Residence';
 
 @Component({
   selector: 'app-residence-details',
@@ -8,13 +9,21 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ResidenceDetailsComponent implements OnInit {
   id!: number;
+  residence!: Residence | undefined;
+    listResidences: Residence[] = [
+      { id: 1, name: "El fel", address: "Borj Cedria", image: "../../assets/img/1.jpg", status: "Disponible" },
+      { id: 2, name: "El yasmine", address: "Ezzahra", image: "../../assets/img/2.jpg", status: "Disponible" },
+      { id: 3, name: "El Arij", address: "Rades", image: "../../assets/img/3.jpg", status: "Vendu" },
+      { id: 4, name: "El Anber", address: "inconnu", image: "../../assets/img/4.jpg", status: "En Construction" }
+    ];
   constructor(private ar: ActivatedRoute) {}
 
   ngOnInit() {
     //this.id = this.ar.snapshot.params['id'];
     //console.log(this.id);
     this.ar.params.subscribe(params => {
-      this.id = params['id'];
+      this.id = +params['id']; // Conversion en nombre
+      this.residence = this.listResidences.find(res => res.id === this.id);
       console.log(this.id);
     });
     }
