b104cc0a4167493f03b68bf3f162b721bdf4c6fb
Yassine Trabelsi <yassine.trabelsi@esprit.tn>
2025-02-22 13:01:36 +0100

    All champs required + 2 champs numbers + surface terrace

--- src/app/Appartment/add-appartment/add-appartment.component.ts
+++ src/app/Appartment/add-appartment/add-appartment.component.ts
@@ -8,13 +8,22 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AddAppartmentComponent {
 
-  AddAppart = new FormGroup({ 
-  apartNum:new FormControl('',Validators.required),
-  floorNum:new FormControl('',[Validators.required,Validators.min(2)]),
-  surface:new FormControl(''),
-  terrace:new FormControl('yes'),
-  surfaceterrace:new FormControl(''),
-  category:new FormControl(''),
-  ResidenceId:new FormControl('')
+  AddAppart = new FormGroup({
+  apartNum: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+  floorNum: new FormControl('', [Validators.required, Validators.min(2), Validators.pattern('^[0-9]+$')]),
+  surface:new FormControl('',Validators.required),
+  terrace:new FormControl('yes',Validators.required),
+  surfaceterrace: new FormControl({ value: '', disabled: false }, Validators.required),
+  category:new FormControl('',Validators.required),
+  ResidenceId:new FormControl('',Validators.required)
 });
+
+onTerraceChange() {
+  if (this.AddAppart.get('terrace')?.value === 'yes') {
+    this.AddAppart.get('surfaceterrace')?.enable();
+  } else {
+    this.AddAppart.get('surfaceterrace')?.disable();
+    this.AddAppart.get('surfaceterrace')?.setValue('');
+  }
+}
 }
