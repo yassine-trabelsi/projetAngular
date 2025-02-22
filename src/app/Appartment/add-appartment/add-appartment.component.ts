3900590ce7adc82a1b456d621f66ea909d36ce40
Yassine Trabelsi <yassine.trabelsi@esprit.tn>
2025-02-22 17:39:59 +0100

    récupération données dans newApart + Bouton reset

--- src/app/Appartment/add-appartment/add-appartment.component.ts
+++ src/app/Appartment/add-appartment/add-appartment.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Appartment } from 'src/core/models/Appartment';
 
 @Component({
   selector: 'app-add-appartment',
@@ -7,6 +8,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./add-appartment.component.css']
 })
 export class AddAppartmentComponent {
+  newApart!: Appartment;
 
   AddAppart = new FormGroup({
   apartNum: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
@@ -14,7 +16,7 @@ export class AddAppartmentComponent {
   surface:new FormControl('',Validators.required),
   terrace:new FormControl('yes',Validators.required),
   surfaceterrace: new FormControl({ value: '', disabled: false }, Validators.required),
-  category:new FormControl('',Validators.required),
+  category:new FormControl('S+1',Validators.required),
   ResidenceId:new FormControl('',Validators.required)
 });
 
@@ -26,4 +28,34 @@ onTerraceChange() {
     this.AddAppart.get('surfaceterrace')?.setValue('');
   }
 }
+
+onSubmit() {
+  if (this.AddAppart.valid) {
+    this.newApart = {
+      apartNum: Number(this.AddAppart.get('apartNum')?.value),
+      floorNum: Number(this.AddAppart.get('floorNum')?.value),
+      surface: Number(this.AddAppart.get('surface')?.value),
+      terrace: this.AddAppart.get('terrace')?.value === 'yes',
+      surfaceterrace: this.AddAppart.get('surfaceterrace')?.value ? Number(this.AddAppart.get('surfaceterrace')?.value) : 0,
+      category: this.AddAppart.get('category')?.value || '',
+      ResidenceId: Number(this.AddAppart.get('ResidenceId')?.value),
+    };
+
+    console.log('New Apartment:', this.newApart);
+  }
+}
+
+resetForm() {
+  this.AddAppart.reset({
+    apartNum: '',
+    floorNum: '',
+    surface: '',
+    terrace: 'yes',
+    surfaceterrace: '',
+    category: 'S+1',
+    ResidenceId: ''
+  });
+  this.onTerraceChange();
+}
+
 }
